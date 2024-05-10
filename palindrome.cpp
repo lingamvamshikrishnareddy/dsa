1e5f0278670db5d0d1a2071b23f06ca7a8280e62
lingamvamshikrishnareddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2024-05-10 00:00:07 -0700

    Check a number is palindrome or not

---
+++ palindrome.cpp
@@ -0,0 +1,34 @@
+// check the given number is palindrome or not
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ispalindrome(int n)
+{
+    int temp= n;
+    int answer{0};
+    while(n>0)
+    { 
+        answer=answer*10+n%10;
+        n/=10; 
+    }
+    if(answer==temp)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+
+}
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<ispalindrome(n);
+}
